d99bd3138ae98e2bcfb73a13cbeea43568458829
PasinduManeka <manekaherat815@gmail.com>
2021-12-20 15:09:32 +0530

    First commit

---
+++ Model/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+        public String Author { get; set; }
+    }
+}
