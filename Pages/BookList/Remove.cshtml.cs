9b15a8bde7628791e1659e0c6eb6e203b0c15bf1
PasinduManeka <manekaherat815@gmail.com>
2021-12-22 00:03:48 +0530

    Fixed the delete function

--- Pages/BookList/Remove.cshtml.cs
+++ Pages/BookList/Remove.cshtml.cs
@@ -5,50 +5,42 @@ using System.Threading.Tasks;
 using BookListRazor.Model;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 
 namespace BookListRazor.Pages.BookList
 {
-    public class indexModel : PageModel
+    public class RemoveModel : PageModel
     {
         //create a object of the applicationDBContext for databse connection
         private readonly ApplicationDBContext _db;
 
         //initialize the constructor. Use the dependency injection
-        public indexModel(ApplicationDBContext db)
+        public RemoveModel(ApplicationDBContext db)
         {
             _db = db;
         }
 
-        //return the books as a list
-        public IEnumerable<Book> Books { get; set; }
+        //Model
+        [BindProperty]
+        public Book Book { get; set; }
 
-        public async Task OnGet()
-        {
-            //set the all the books from database
-            //Async let you run mutiple tasks until it is waited
-            Books = await _db.Book.ToListAsync();
-        }
-
-        public async Task<IActionResult> OnPost(int id)
+        public async Task<IActionResult> OnGet(int id)
         {
             //Retrieve the book data from database
-            var book = await _db.Book.FindAsync(id);
+            var book =  await _db.Book.FindAsync(id);
 
             //check the that book available or not
             if (book == null)
             {
-                return NotFound();
+                NotFound();
             }
 
             //remove the book
             _db.Book.Remove(book);
-            
+
             //save the changes in the databse
             await _db.SaveChangesAsync();
+            return Redirect("Index");
 
-            //redirect to a page
-            return RedirectToPage("Index");
         }
     }
 }
